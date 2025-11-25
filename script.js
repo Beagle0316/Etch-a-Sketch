277dc24a3087f6db677a16155873f86506e9d4ae
Beagle0316 <twynsta.beagle@gmail.com>
2025-11-25 19:42:32 +0800

    Apply flex-wrap to grid cells

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function squareDivs(){
     const container = document.querySelector(".container");
     
+    for (let i = 0; i < 16; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        container.appendChild(cell);
+    }
+    
 }
+
+squareDivs();
