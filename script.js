1a5c2f51121ed74fd3c044f1ac6c0e21056f0883
Beagle0316 <twynsta.beagle@gmail.com>
2025-11-25 20:37:50 +0800

    Add prompt feature

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function squareDivs(){
+function squareDivs(numberOfCells){
     const container = document.querySelector(".container");
     
-    for (let i = 0; i < 272; i++){
+    for (let i = 0; i < numberOfCells; i++){
         const cell = document.createElement("div");
         cell.classList.add("cell");
         container.appendChild(cell);
     }
+
+    hover();
     
 }
 
@@ -19,5 +21,22 @@ function hover(){
     });
 }
 
-squareDivs();
-hover();
+function main(){
+    const button = document.querySelector(".brushSize");
+
+    button.addEventListener("click", () =>{
+        let newCells = prompt("Enter number of cells:");
+
+        if (newCells !== null){
+            if (newCells <= 0){
+                alert("Invalid input");
+            }
+        }
+    });
+
+}
+
+// Start of Program
+let numberOfCells = 272;
+squareDivs(numberOfCells);
+main();
